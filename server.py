5230643075f6274b6ae0e76d43ed84ca49ac3f13
夢見りあむ <kengsini250@gmail.com>
2020-02-06 11:07:18 +0900

    v1.0

--- server.py
+++ server.py
@@ -2,26 +2,48 @@ import socket
 import time
 import sys
 
-HOST_IP = socket.gethostname()
-HOST_PORT = 55555
-print("Start socket")
+class Server:
+    __yes = 'DY'
+    __no = 'DN'
 
-socket_tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("TCP server listen @%s:%d!" %(HOST_IP,HOST_PORT))
-host_addr = (HOST_IP,HOST_PORT)
-socket_tcp.bind(("",HOST_PORT))
-socket_tcp.listen(1)
+    def __init__(self):
+        self.ip = ""
+        self.port = -1
 
-yes = 'DY'
-no = 'DN'
+    def __init__ (self,ip,port):
+        self.ip = ip
+        self.port = port
 
-while True:
-    client,host_addr=socket_tcp.accept()
-    print("Connection acceptd tfrom %s"%host_addr[0])
-    while True:
-        client.send(yes.encode('utf-8'))
-        time.sleep(1)
-        client.send(no.encode('utf-8'))
-        time.sleep(1)
-socket_tcp.close()
-print("server end")
+    def __del__(self):
+        self.server.close()
+        print("server end")
+
+    def getIP(self):
+        return self.ip
+    def getPORT(self):
+        return self.port
+    def getServerData(self):
+        print("ip : %s" %self.getIP())
+        print("port : %s" %self.getPORT())
+
+    def serverInit(self,MAX):
+        print("Start socket")
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.getServerData()
+        self.host_addr = (self.getIP(),self.getPORT())
+        self.server.bind((self.getIP(),self.getPORT()))
+        self.server.listen(MAX)
+
+    def serverStart(self):
+        self.client,self.host_addr=self.server.accept()
+        print("Connection acceptd tfrom %s"%self.host_addr[0])
+        while True:
+            self.client.send(self.__yes.encode('utf-8'))
+            time.sleep(1)
+            self.client.send(self.__no.encode('utf-8'))
+            time.sleep(1)
+
+
+server = Server(socket.gethostname(),55555)
+server.serverInit(5)
+server.serverStart()
